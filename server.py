577fe9db35f80b3ae04bbb1ff29df5ddec34d350
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2021-12-22 22:36:51 -0700

    Started Assignment

--- server.py
+++ server.py
@@ -1,1 +1,27 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = "secrets don't make friends"
 
+@app.route('/')
+def index():
+	return render_template("index.html")
+
+@app.route('/route')
+def atoute():
+	pass
+
+@app.route('/route')
+def atoute():
+	pass
+
+@app.route('/route')
+def atoute():
+	pass
+
+# no page found
+@app.errorhandler(404)
+def pageNotFound(missing):
+	return "Sorry! No response. Try again."
+
+if __name__=="__main__":
+	app.run(debug=True)
