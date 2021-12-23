d835f9f4e11701f726ed33c97e2a5b92d02765ba
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2021-12-23 14:59:47 -0700

    finished assignment

--- server.py
+++ server.py
@@ -1,22 +1,42 @@
 from flask import Flask, render_template, request, redirect, session
+import random
+
 app = Flask(__name__)
 app.secret_key = "secrets don't make friends"
 
 @app.route('/')
 def index():
-	return render_template("index.html")
+    if 'randomNum' in session:
+        pass
+    else:
+        session['randomNum'] = random.randint(1,100)
+    if 'numTries' in session:
+        pass
+    else:
+        session['numTries'] = 0
+    if 'guess' in session:
+        pass
+    else:
+        if request.form.get('guess'):
+            session['guess'] = int(request.form['guess'])
+        else:
+            session['guess'] = -1
+    return render_template("index.html")
 
-@app.route('/route')
-def atoute():
-	pass
+@app.route('/play_again')
+def play_again():
+    session.clear()
+    return redirect("/")
 
-@app.route('/route')
-def atoute():
-	pass
-
-@app.route('/route')
-def atoute():
-	pass
+@app.route('/guess', methods=['POST'])
+def guess():
+    if 'numTries' in session:
+        session['numTries'] += 1
+    else:
+        session['numTries'] = 0
+    session['guess'] = int(request.form['guess'])
+    session['randomNum'] = int(session['randomNum'])
+    return redirect("/")
 
 # no page found
 @app.errorhandler(404)
